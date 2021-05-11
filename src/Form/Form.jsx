e9f38f31dbeaf3c603116dd61a04a43ae7474d6f
brian rivera <50382499+BrianRivera@users.noreply.github.com>
2021-05-11 01:22:12 -0400

    webiando uwu

---
+++ src/Form/Form.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import './Form.scss'
+import asdlkj from './../assets/jhghjk.jpg'
+
+export const Form = () => {
+
+  const [form, setForm] = useState({
+    name: 'Sebastian',
+    lastName: 'Godoy',
+    year: 13
+  })
+  const {name, lastName, year} = form
+
+  const inputGroup = [{
+    title: 'name',
+    type: 'text'
+  },
+  {
+    title: 'lastName',
+    type: 'text'
+  },
+  {
+    title: 'year',
+    type: 'number'
+  }]
+
+
+  const setFormValue = (e) => setForm({...form, [e.target.name]: e.target.value})
+  
+const handleClick = () => alert(`holi me llamo ${name} ${lastName} y tengo ${year} uwu <3`);
+
+  return (
+    <>
+    <div className="">
+    <div className="formContainer">
+    <div className="formSeccion">
+      {
+        inputGroup.map((e,i)=>(<div className='form' key={i}>
+          <p>{e.title}</p>
+          <input type={e.type}
+           name={e.title}
+           onChange={setFormValue}
+           value={form[e.title || '']}/>
+        </div>))
+      }
+    </div>
+    <div className="formSeccion">
+      <img src={asdlkj} alt="" />
+    </div>
+    </div>
+    <div className="buttonContainer">
+      <button onClick={handleClick}>uwu</button>
+    </div>
+    </div>
+    </>
+  )
+}
